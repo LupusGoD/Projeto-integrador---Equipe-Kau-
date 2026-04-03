1bf66332963d2d0beefbffcad103c7628ec2cde8
kaua cecilio <kauac24@outlook.com>
2026-04-02 21:19:10 -0300

    Criação das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import './App.css'
-import './Style/style.css'
-import Cabecalho from './componentes/Cabecalho'
+import {Route, Routes} from 'react-router-dom'
 import Home from './paginas/Home'
-import Projetos from './paginas/Projetos'
 import Cadastro from './paginas/Cadastro'
-import DetalhesProjeto from './componentes/DetalhesProjeto'
-import Cards from './componentes/Cards'
-import BarraDeBusca from './componentes/BarraDeBusca'
-import Filtro from './componentes/Filtro'
-import Rodape from './componentes/Rodape'
+import Projetos from './paginas/Projetos'
+import './Style/style.css'
+
 
 function App() {
 
   return (
-    <div>
-      <Cabecalho />
-      <Home />
-      <Projetos />
-      <Cadastro />   
-      <DetalhesProjeto />
-      <Cards />
-      <BarraDeBusca />
-      <Filtro />
-      <Rodape />
-
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/cadastro" element={<Cadastro />} /> 
+        <Route path="/projetos" element={<Projetos />} /> 
+      </Routes>
+    </>
   )
 }
 
--- src/paginas/Home.tsx
+++ src/paginas/Home.tsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h2>Página Home</h2>
+            <h2>Seja Bem vindo a Home!</h2>
         </div>
     )
 }
